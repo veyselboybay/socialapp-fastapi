af1e2115ce2dbdb52911aecb980d79dce6c87a36
veyselboybay <veysel.boybay@outlook.com>
2023-08-19 14:03:52 -0400

    Post db model and schema is created

--- app/models/models.py
+++ app/models/models.py
@@ -2,6 +2,18 @@ from sqlalchemy import Column,String,Integer,ForeignKey,Boolean
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.sql.expression import text
 from ..database import Base
+from sqlalchemy.orm import relationship
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer,primary_key=True,nullable=False)
+    title = Column(String,nullable=False)
+    content = Column(String,nullable=False)
+    published = Column(Boolean,server_default='True')
+    created_at = Column(TIMESTAMP(timezone=True),server_default=text('now()'))
+    owner_id = Column(Integer,ForeignKey("users.id",ondelete='CASCADE'))
+    owner = relationship("User")
 
 class User(Base):
     __tablename__ = "users"
--- app/schemas/user_schemas.py
+++ app/schemas/user_schemas.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel, EmailStr
 from datetime import datetime
 
+
+
 class UserCreate(BaseModel):
     email: EmailStr
     password: str
@@ -9,6 +11,32 @@ class UserOut(BaseModel):
     id: int
     email: EmailStr
     created_at: datetime
+
+    class Config:
+        from_attributes = True
+
+# Post Schemas
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+class PostCreate(PostBase):
+    pass
+
+class Post(PostBase):
+    id: int
+    owner_id : int
+    created_at: datetime
+    owner: UserOut
+
+    class Config:
+        from_attributes = True
+
+class PostOut(BaseModel):
+    Post: Post
+    votes: int
 
     class Config:
         from_attributes = True
